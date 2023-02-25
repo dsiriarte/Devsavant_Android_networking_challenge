a59750811d20481f666812282506609b6acc7d93
santiagoiriarte <davidsantiagoiriarte@gmail.com>
2023-02-25 12:37:28 -0500

    initial commit

---
+++ app/src/main/java/com/davidsantiagoiriarte/devsavant_android_networking_challengepublic/domain/Character.kt
@@ -0,0 +1,15 @@
+package com.davidsantiagoiriarte.devsavant_android_networking_challengepublic.domain
+data class Character(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String?,
+    val gender: String,
+    val origin: Location,
+    val location: Location,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String
+)
---
+++ app/src/main/java/com/davidsantiagoiriarte/devsavant_android_networking_challengepublic/domain/Location.kt
@@ -0,0 +1,6 @@
+package com.davidsantiagoiriarte.devsavant_android_networking_challengepublic.domain
+
+data class Location(
+    val name: String,
+    val url: String
+)
